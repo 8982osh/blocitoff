89d2d977f8a87de0167071b47096888de52c67c3
Cande Garcia <cruztoyota@gmail.com>
2015-12-24 22:51:08 -0600

    Module 2 sign up completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  get 'welcome/about'
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  
   # You can have the root of your site routed with "root"
   root to: 'welcome#index'
 
